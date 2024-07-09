e58a795f4a095280e0841e18ca4de70f1245527d
Soniya <soniyasoni3331@gmail.com>
2024-07-09 23:46:58 +0530

    components updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import NewsItem from '../NewsItem/NewsItem'
+import React, { useEffect, useState } from 'react'
+import NewsItem from '../NewsItem/NewsItem.jsx'
 
-function Header() {
+
+
+
+function Home() {
+    const [articles, setArticles] = useState([]);
+    const fetchData = async () =>{
+        const URL = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8276e6b5404a4373b532af4c00b22ff8`//todo -- api key hide
+        const reponse = await fetch(URL);
+        const data = await reponse.json();
+        console.log(data.articles[0].title);   
+         setArticles(data.articles);
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+
   return (
-    <div>
-      <NewsItem/>
+    <div className='w-full h-auto bg-[#f0c647] flex justify-evenly py-5 m-auto'>
+      {
+        articles.map((item)=>{
+            <NewsItem title={item.title} description={item.description} image={item.imageUrl} authorName={item.urlToImage} />
+        })
+      }
     </div>
   )
 }
 
-export default Header
+export default Home
--- src/components/NewsItem/NewsItem.jsx
+++ src/components/NewsItem/NewsItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function NewsItem() {
+function NewsItem({description, title, image, authorName}) {
   return (
-    <div className='w-1/4 h-[350px] border border-yellow-400 bg-yellow-300 rounded-sm'>
-        <div>
-            <img src="" alt="" />
+    <div className='w-1/5 h-[300px] border border-black bg-black text-white rounded-md hover:scale-105 duration-150'>
+        <div className='flex flex-col justify-between items-center'>
+            <img src={image} alt="news" />
             <div>
-                <h5 >title</h5>
-                <p>description</p>
+                <h5>{title}</h5>
+                <p>{description}</p>
                 <a href="#">go somewhere</a>
-                <p>author name</p>
+                <p>{authorName}</p>
             </div>
         </div>
     </div>
